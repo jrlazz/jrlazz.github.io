fef1fc1f9d7fea61d97ef7a41589e3fbff7fdbf4
jrlazz <50157295+jrlazz@users.noreply.github.com>
2025-09-02 15:21:46 -0300

    Add files via upload

---
+++ anim/ggk_car.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>ggk_car.php</title>
+<meta charset="utf-8">
+<link rel="shortcut icon" href="../three.js.ico">
+<link type="text/css" rel="stylesheet" href="main.css">
+
+<style>
+
+body{margin:0;background-color:#012;color:#ff0;font-family:Verdana;font-size:13px;overscroll-behavior:none;line-height:20px;overflow:hidden;}
+
+</style>
+
+</head>
+
+<body>
+
+<script>
+
+window.location.replace("http://jrlazz.eu5.org/ggk/ggk_car.php");
+
+</script>
+
+</body>
+
+</html>
---
+++ anim/heli.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>heli.php</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
+<link rel="shortcut icon" href="../three.js.ico">
+<link type="text/css" rel="stylesheet" href="main.css">
+<style>
+body {background-color:#000000;color:#ff0;}
+a{color: #2983ff;}
+</style>
+</head>
+
+<body>
+
+<script>
+window.location.replace("http://jrlazz.eu5.org/ggk/heli_res_res.php");
+</script>
+
+</body>
+</html>
